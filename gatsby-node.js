4330037914af6512523f3c91db0efc2d54de9baf
Gavin Mogan <git@gavinmogan.com>
2022-12-10 20:08:17 -0800

    Fix: dont hardcode webcomponents preview

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,6 @@
 const {makeReactLayout, saveReactLayout} = require('./makeLayout');
 
 exports.onPreBootstrap = async (_, pluginOptions) => {
-    pluginOptions.headerUrl = 'https://deploy-preview-5668--jenkins-io-site-pr.netlify.app/template/index.html';
-    console.log('pluginOptions', pluginOptions);
     await makeReactLayout(pluginOptions).then(saveReactLayout);
 };
 
